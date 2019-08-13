014fb69757f659f6f8ff1ad382035c82c72d25f7
Luke Miller <luke@snowjeweldesign.com>
2019-08-13 15:30:53 -0600

    more style/copy changes, update to email validation

--- static/source/javascripts/main.js
+++ static/source/javascripts/main.js
@@ -4,23 +4,36 @@
     domready(function () {
         submit_button = document.getElementById('submit_button');
         email_input = document.getElementById('email_input');
-        email_input.onkeydown = keypressHandler;
-        email_input.onclick = keypressHandler;
+        submit_button.onclick = validateForm;
+        // email_input.onclick = keypressHandler;
         // email_input.change = keypressHandler;
     });
     
-    
-    function keypressHandler(e) {
-        console.log(this.value)
+    function validateForm(e) {
+        const value = email_input.value;
         const error_display = document.getElementById('errors');
-        if (emailIsValid(this.value)) {
+
+        if (emailIsValid(value)) {
+            return true;
+        }
+        error_display.innerText = 'Email address is invalid.'
+        return false;
+    }
+
+    function keypressHandler(e) {
+        const value = this.value;
+        const error_display = document.getElementById('errors');
+        if (emailIsValid(value)) {
             submit_button.disabled = false;
             error_display.innerText = '';
-        } else {
-            error_display.innerText = 'Email address is invalid.'
+        } else if (value.length < 6) {
             submit_button.disabled = true;
+        } else {
+            submit_button.disabled = true;
+            error_display.innerText = 'Email address is invalid.'
         }
     }
+
     function emailIsValid (email) {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
     }
